6492500921c0e49c99c2b6dca59c830ce53b1c0e
max-ellis <mjellis@ualberta.ca>
2020-03-02 19:32:51 -0700

    reset for semantics test1

---
+++ src/main/java/main.java
@@ -0,0 +1,53 @@
+
+
+public class main {
+
+    public static void main(String args[]){
+		A a = new A();
+		a.method1();
+    }
+
+}
+
+
+class A {
+
+	void method1() {
+		int var1 = 0;
+		int variable2 = 5;
+		
+		B classB = new B();
+
+		int var3 = classB.method2(var1, variable2);
+		int var4 = classB.add(var3, var3);
+	}
+
+	int method(int a) {
+		return a;
+
+	}
+
+
+}
+
+
+class B {
+
+	int method2(int a, int b) {
+		int variable2 = a + b;
+
+		A classA = new A();
+
+		variable2 = classA.method(variable2);
+		return variable2;
+	}
+
+	int add(int a, int b) {
+		int var = a + b;
+
+		A classA = new A();
+
+		var = classA.method(var);
+		return var + var;
+	}
+}
